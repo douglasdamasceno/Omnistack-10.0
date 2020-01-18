ba3fc52c586ca289cecfaf28222494b70c7cca58
douglasdm <douglasmdamasceno@gmail.com>
2020-01-18 19:00:34 -0300

    versão web finalizada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-4pljl.mongodb.net/week10?retryWrites=true&w=majority',
 {
     useNewUrlParser:true,
     useUnifiedTopology:true,
+    useCreateIndex: true,
     
 });
 
+app.use(cors({origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,7 @@
 import React ,{useEffect,useState}from 'react';
-
+import api from './services/api';
+import DevItem from './components/DevItem/index';
+import DevForm from './components/DevForm/index';
 import './global.css'
 import './App.css'
 import './sidebar.css'
@@ -7,102 +9,35 @@ import './main.css'
 
 
 function App() {
-  const [latitude, setLatitude] = useState(''); 
-  const [longitude, setLongitude] = useState(''); 
-  
+  const [devs,setDevs] = useState([]);
+
   useEffect(()=>{
-    navigator.geolocation.getCurrentPosition(
-      (position)=>{
-        const {latitude,longitude} = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err)=>{
-        console.log(err);
-      },{
-        timeout:3000,
-      }
-    );
-  },[]);  
+    async function loadDevs(){
+      const response = await api.get('./devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  },[]);
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs',data);
+   
+    setDevs([...devs,response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário de Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="number" name="latitude" id="latitude"
-                required value={latitude}
-                onChange={e=> setLatitude(e.target.value)}/>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="number" name="longitude" id="longitude" 
-                required value={longitude} 
-                onChange={e=> setLongitude(e.target.value)}/>
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>        
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-              alt="foto do perfil" />
-              <div className="user-info">
-                <strong>Diego</strong>
-                <span>ReactJS,React native</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-            <a href="https://github.com/diego3g" >Acessar Perfil do Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-              alt="foto do perfil" />
-              <div className="user-info">
-                <strong>Diego</strong>
-                <span>ReactJS,React native</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-            <a href="https://github.com/diego3g" >Acessar Perfil do Github</a>
-          </li><li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-              alt="foto do perfil" />
-              <div className="user-info">
-                <strong>Diego</strong>
-                <span>ReactJS,React native</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-            <a href="https://github.com/diego3g" >Acessar Perfil do Github</a>
-          </li><li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-              alt="foto do perfil" />
-              <div className="user-info">
-                <strong>Diego</strong>
-                <span>ReactJS,React native</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-            <a href="https://github.com/diego3g" >Acessar Perfil do Github</a>
-          </li>
+          {devs.map(dev=>{
+            return(
+              <DevItem key={dev._id}  dev={dev}/>
+            )
+          })}
         </ul>
       </main>
     
